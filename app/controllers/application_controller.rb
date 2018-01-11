2b7cb69036e10fbb3f7122b29f9aab4fa82de38d
Sonialanglet <slanglet@loeildumarketing.com>
2018-01-11 12:23:38 +0100

    message de pndit changé dans l'applicationcontroller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,9 @@
 class ApplicationController < ActionController::Base
+  include Pundit
   protect_from_forgery with: :exception
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
-  include Pundit
+
 
     # Pundit: white-list approach.
     after_action :verify_authorized, except: :index, unless: :skip_pundit?
@@ -18,10 +19,15 @@ class ApplicationController < ActionController::Base
 
     # Uncomment when you *really understand* Pundit!
      # rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
-     # def user_not_authorized
-     #   flash[:alert] = "You are not authorized to perform this action."
-     #   redirect_to(root_path)
-     # end
+
+     def user_not_authorized
+       flash[:alert] = "You are not authorized to perform this action."
+       redirect_to(root_path)
+     end
+
+     def default_url_options
+       { host: ENV["HOST"] || "localhost:3000" }
+     end
 
 
      private
