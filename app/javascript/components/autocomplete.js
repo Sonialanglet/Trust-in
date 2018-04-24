66fb596cf418f520a7f31eea693e959d1783fb25
Sonialanglet <slanglet@loeildumarketing.com>
2018-04-24 19:11:42 +0200

    changement de adress en string pour l'autocomplete

--- app/javascript/components/autocomplete.js
+++ app/javascript/components/autocomplete.js
@@ -1,6 +1,6 @@
 function autocomplete() {
   document.addEventListener("DOMContentLoaded", function() {
-    var profileAddress = document.getElementById('profile_town');
+    var profileAddress = document.getElementById('profile_adress');
 
     if (profileAddress) {
       var autocomplete = new google.maps.places.Autocomplete(profileAddress, { types: [ 'geocode' ] });
