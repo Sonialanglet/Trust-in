d90477092ab0ebebc959c0140d5b54dba3c3b4fb
Sonialanglet <slanglet@loeildumarketing.com>
2018-04-30 17:06:59 +0200

    forest et index de prestations design

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
 
 
+  mount ForestLiana::Engine => '/forest'
   devise_for :users,
       controllers: { registrations: 'users/registrations', omniauth_callbacks: 'users/omniauth_callbacks' }
 
