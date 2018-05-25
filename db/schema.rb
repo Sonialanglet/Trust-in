e11b1c958d8b4ab0a6017e52586f72d5dd88d03c
Sonialanglet <slanglet@loeildumarketing.com>
2018-05-25 15:23:58 +0200

    rails admin installé

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180503134747) do
+ActiveRecord::Schema.define(version: 20180525130409) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -199,6 +199,7 @@ ActiveRecord::Schema.define(version: 20180503134747) do
     t.string "facebook_picture_url"
     t.string "token"
     t.datetime "token_expiry"
+    t.boolean "admin", default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
