601b6e642f97cd1249c5b765758bd9123cb5adc7
Sonialanglet <slanglet@loeildumarketing.com>
2018-11-22 15:14:50 +0100

    ok le test de stripe et ok le passage par la page d'identification

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -6,12 +6,13 @@ class Users::RegistrationsController < Devise::RegistrationsController
    resource.update_without_password(params)
   end
 
-  # def after_sign_up_path_for(resource)
-  #   super(resource)
+  def after_sign_up_path_for(resource)
+    super(resource)
+    return identification_page_path
   #   group=Group.new(title: "carnet d'adresse de #{resource.email}", founder: resource)
   #   group.users << resource
   #   group.save
-  #end
+  end
 
 
   def registration_paramss
