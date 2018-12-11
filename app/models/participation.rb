3e75659b01d1da62239ca7544019502a3e7416da
Sonialanglet <slanglet@loeildumarketing.com>
2018-12-11 16:23:41 +0100

    modif de la valeur par defaut de la quantité

--- app/models/participation.rb
+++ app/models/participation.rb
@@ -1,4 +1,5 @@
 class Participation < ApplicationRecord
 
   belongs_to :user
+  validates :quantity, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181211085916) do
+ActiveRecord::Schema.define(version: 20181211151733) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -97,7 +97,7 @@ ActiveRecord::Schema.define(version: 20181211085916) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "quantity"
+    t.integer "quantity", default: 0, null: false
     t.string "event_ref"
     t.jsonb "payment"
     t.integer "amount_cents", default: 0, null: false
